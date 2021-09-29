527db1a27ce7e35aa5ed75032e6c5333b587a9b7
montanarey <montana.pistell@hotmail.com>
2021-09-28 21:55:11 -0700

    Add base html

---
+++ calculator.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Addition Only Calculator</title>
+</head>
+<body>
+    <h1>Addition Only Calculator</h1>
+</body>
+</html>
