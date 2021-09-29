fe11f7e40d7954fe6610377126f3780afbeba22c
montanarey <montana.pistell@hotmail.com>
2021-09-28 22:11:18 -0700

    Add documentation and create form fields

--- calculator.php
+++ calculator.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+    <!--
+    DGL123 Introduction to PHP
+    Author: Montana Pistell
+    Last Modified: September 29, 2021
+    Purpose: Week 04 Assignment. Allows a user to input two numeric values, and outputs the sum of those values.
+ -->
 <head>
     <meta charset="UTF-8">
     <title>Addition Only Calculator</title>
 </head>
 <body>
     <h1>Addition Only Calculator</h1>
+
+    <!-- HTML form to gather user input-->
+    <form action="calculator.php" method="post">
+        <label for="firstNum">First Number:</label>
+        <input type="text" name="firstNum" id="firstNum" required>
+        <br>
+        <label for="secondNum">Second Number:</label>
+        <input type="text" name="secondNum" id="secondNum" required>
+        <br>
+        <input type="submit" name="submit" value="Calculate">
+    </form>
+
 </body>
 </html>
